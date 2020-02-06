2b49cadea6c1660fc1c19e357045ba1c42417822
Baptiste Morel <baptiste.morel82170@gmail.com>
2020-02-06 16:35:11 +0100

    Create exo3.c

---
+++ exo3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+int main(){
+
+typedef struct Lieu LIEU;
+struct LIEU{
+
+char nomLieu[50];
+char descLieu[100];
+int diff;
+int destination;
+};
+
+LIEU Liste [3];
+
+liste[0].nomLieu="Paris";
+liste[0].descLieu="Ville en france";
+liste[0].diff=19;
+liste[0].destination=1;
+
+
+liste[1].nomLieu="Montreal";
+liste[1].descLieu="Ville au Quebec";
+liste[1].diff=2;
+liste[1].destination=2;
+
+liste[2].nomLieu="Londre";
+liste[2].descLieu="Ville en Angleterre";
+liste[2].diff=10;
+liste[1].destination=0;
+
+
+
+return 0;
+}
